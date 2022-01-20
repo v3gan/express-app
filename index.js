c33fadefe7444eb5bcd9c007e6f1f438f28c228e
marcus <marcmonk@gmail.com>
2022-01-20 14:18:36 -0500

    02-02 add data to the server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import data from './data/data.json'
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+    console.log(data);
+});
