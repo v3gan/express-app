d2bf785bc618c4715a371c5b5d0820d6401705e1
marcus <marcmonk@gmail.com>
2022-01-20 20:52:42 -0500

    04-02 built in middleware

--- index.js
+++ index.js
@@ -7,16 +7,41 @@ const PORT = 3000;
 // this is for the public folder on path /
 app.use(express.static("public"));
 
+// method to use JSON
+//app.use(express.json());
+
+// method to use URLEncoded
+app.use(express.urlencoded({extended: true}));
+
 // this is for the images folder on path /images
 app.use("/images", express.static("images"));
 
 app.get("/", (req, res) => res.json(data));
 
+// JSON data
+// {"hello": "JSON is cool"}
+// URLEncoded data
+// hello=URLEncoded+is+cool
+
+app.post('/newItem', (req, res) => {
+    console.log(req.body);
+    res.send(req.body)
+})
+
+// next is the second arrow function
+// you can chain as many next() functions as you want before or after the res.send() call
+// **there can only be 1 res.send() call**
 app.get(
   "/item/:id",
   (req, res, next) => {
+    // this is the middleware that pulls the data
     let userId = Number(req.params.id);
     const user = data.filter((u) => u.id == userId)[0];
+    // middleware that uses the request object
+    console.log(`request from: ${req.originalUrl}`);
+    console.log(`request type ${req.method}`);
+    // everything above is middleware
+    // express.js.com/en/guide/using-middleware.html
     res.send(user);
     next();
   },
